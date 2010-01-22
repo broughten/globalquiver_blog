e4197a5361ea5e8e36b59b773a98d4241467e231
Jonathan Christensen <jc@kelsus.local>
2010-01-22 14:48:48 -0700

    added a theme and remove wp-config.php

---
+++ wp-content/themes/rails-generated/index.php
@@ -0,0 +1,108 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-type" />
+    <title> </title>
+    <link href="/stylesheets/main.css?1263860641" rel="stylesheet" type="text/css" media="screen" />
+<link href="/stylesheets/authentication.css?1263965460" rel="stylesheet" type="text/css" media="screen" />
+    <!--[if IE]>
+      <link href="/stylesheets/ie.css?1263453130" media="screen" rel="stylesheet" type="text/css" />      
+    <![endif]-->
+	
+	<script src="http://www.google.com/jsapi"></script>
+	<script>
+		// Load jQuery
+		google.load("jquery", "1.3.2");
+	</script>
+    
+	
+	<script>
+          
+          $(document).ready(function() {
+            setTimeout(function() {
+              $("div.flash").fadeOut('fast');
+            }, 3000);
+          });
+	</script>
+  </head>
+  <body class="blog_themes blog_themes-show">
+    <div id="outer-container">
+      <div id="header">
+        <div class="left">
+          <a href="/"><h1 id="logo"><span>GlobalQuiver</span></h1></a>
+        </div>
+        <div class="right">
+          <div id="login-signup">
+            <ul>
+              		
+                <li id="signup_link"><a href="http://localhost/users/new">Join Global Quiver</a></li><li id="login_link"><a href="http://localhost/session/new">Login</a></li>
+                          </ul>
+          </div>
+        </div>
+      </div>
+      <div class="tabs">
+        <ul>
+          
+            <li><a href="/pages/Home">Home</a></li>
+          
+            <li><a href="/pages/About">About</a></li>
+          
+            <li><a href="/pages/FAQ">Faq</a></li>
+          
+            <li><a href="/blog">Blog</a></li>
+        </ul>
+      </div>
+      <div class="clear"></div>
+      
+        <div id="left-nav">
+          <div class="left-nav-button">
+            <img src="/images/bluearrow.png?1262744906" alt="arrow" />
+            <div class="left-link">
+              <a href="http://localhost/boards/new" id="new_board">Add a board</a>
+            </div>
+          </div>
+          <div class="left-nav-button">
+            <img src="/images/bluearrow.png?1262744906" alt="arrow" />
+            <div class="left-link">
+              <a href="http://localhost/board_searches/new" id="new_search">Find a board</a>
+            </div>
+          </div>
+        </div>
+      
+      <div id="container">
+      
+
+
+<div id="mainContent">
+
+
+
+      <div class="clear"></div>
+        <div id="footer">
+          <p>Copyright &copy; 2010  Global Quiver, Inc.</p>
+          <ul>
+            <li class="last"><a href="mailto:info@kelsusconsulting.com">Found a Bug?</a></li>
+          </ul>
+          <div id="surfer-pic-bottom">
+            <img src="/images/tajburrowpic.jpg" id="taj-burrow-pic" alt="tajburrowpic" />
+          </div>
+        </div>
+      </div><!--Container from header-->
+    </div><!--Outer container from header-->
+      <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+      </script>
+      <script type="text/javascript">
+      try {
+      var pageTracker = _gat._getTracker("");
+      pageTracker._trackPageview();
+      } catch(err) {}</script>
+  
+
+
+
+</div></body></html>
