4877f2b456f052316db17a265514442fa0172e49
Jonathan Christensen <jc@kelsus.local>
2010-01-22 17:24:23 -0700

    small typo

--- wp-content/themes/rails-generated/index.php
+++ wp-content/themes/rails-generated/index.php
@@ -95,7 +95,7 @@
                                                 <span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in ', 'rails-generated' ); ?></span><?php echo get_the_category_list(', '); ?></span>
                                                 <span class="meta-sep"> | </span>
                                                 <?php the_tags( '<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">' . __('Tagged ', 'rails-generated' ) . '</span>', ", ", "</span>\n\t\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-                                                <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'rails-generated' ), __( '1 Comment', 'rails-generated' ), __( '% Comments', 'rails-generated` ) ) ?></span>
+                                                <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'rails-generated' ), __( '1 Comment', 'rails-generated' ), __( '% Comments', 'rails-generated' ) ) ?></span>
                                                 <?php edit_post_link( __( 'Edit', 'rails-generated' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t\n" ) ?>
                                         </div><!-- #entry-utility -->   
                                 </div><!-- #post-<?php the_ID(); ?> -->
