04074cc0d3a742cec46fe6f7797faf32a172a85e
Jonathan Christensen <jc@kelsus.local>
2010-01-24 23:16:05 -0700

    styling it

--- wp-content/themes/rails-generated/single.php
+++ wp-content/themes/rails-generated/single.php
@@ -1,62 +1,85 @@
-<?php get_header(); ?>
-        
-                <div id="blog-container">    
-                        <div id="content">
-                        
-<?php the_post(); ?>
+<?php get_header(); ?>    
+<div id="blog-container">    
+  <div id="content">
 
-                                <div id="nav-above" class="navigation">
-                                        <div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&laquo;</span> %title' ) ?></div>
-                                        <div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&raquo;</span>' ) ?></div>
-                                </div><!-- #nav-above -->
+  <?php the_post(); ?>
 
-                                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                                        <h1 class="entry-title"><?php the_title(); ?></h1>
-                                        
-                                        <div class="entry-meta">
-                                                <span class="meta-prep meta-prep-author"><?php _e('By ', 'your-theme'); ?></span>
-                                                <span class="author vcard"><a class="url fn n" href="<?php echo get_author_link( false, $authordata->ID, $authordata->user_nicename ); ?>" title="<?php printf( __( 'View all posts by %s', 'your-theme' ), $authordata->display_name ); ?>"><?php the_author(); ?></a></span>
-                                                <span class="meta-sep"> | </span>
-                                                <span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'your-theme'); ?></span>
-                                                <span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
-                                                <?php edit_post_link( __( 'Edit', 'your-theme' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>                                               
-                                        </div><!-- .entry-meta -->
-                                        
-                                        <div class="entry-content">
-<?php the_content(); ?>
-<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
-                                        </div><!-- .entry-content -->
-                                        
-                                        <div class="entry-utility">
-                                        <?php printf( __( 'This entry was posted in %1$s%2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%5$s" title="Comments RSS to %4$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'your-theme' ),
-                                                get_the_category_list(', '),
-                                                get_the_tag_list( __( ' and tagged ', 'your-theme' ), ', ', '' ),
-                                                get_permalink(),
-                                                the_title_attribute('echo=0'),
-                                                comments_rss() ) ?>
+    <div id="nav-above" class="navigation">
+      <div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&laquo;</span> %title' ) ?></div>
+      <div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&raquo;</span>' ) ?></div>
+    </div><!-- #nav-above -->
 
-<?php if ( ('open' == $post->comment_status) && ('open' == $post->ping_status) ) : // Comments and trackbacks open ?>
-                                                <?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'your-theme' ), get_trackback_url() ) ?>
-<?php elseif ( !('open' == $post->comment_status) && ('open' == $post->ping_status) ) : // Only trackbacks open ?>
-                                                <?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'your-theme' ), get_trackback_url() ) ?>
-<?php elseif ( ('open' == $post->comment_status) && !('open' == $post->ping_status) ) : // Only comments open ?>
-                                                <?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'your-theme' ) ?>
-<?php elseif ( !('open' == $post->comment_status) && !('open' == $post->ping_status) ) : // Comments and trackbacks closed ?>
-                                                <?php _e( 'Both comments and trackbacks are currently closed.', 'your-theme' ) ?>
-<?php endif; ?>
-<?php edit_post_link( __( 'Edit', 'your-theme' ), "\n\t\t\t\t\t<span class=\"edit-link\">", "</span>" ) ?>
-                                        </div><!-- .entry-utility -->                                                                                                   
-                                </div><!-- #post-<?php the_ID(); ?> -->                 
+    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+
+      <div class="entry-meta">
+        <span class="meta-prep meta-prep-author"><?php _e('By ', 'your-theme'); ?></span>
+        <span class="author vcard"><a class="url fn n" href="<?php echo get_author_link( false, $authordata->ID, $authordata->user_nicename ); ?>" title="<?php printf( __( 'View all posts by %s', 'your-theme' ), $authordata->display_name ); ?>"><?php the_author(); ?></a></span>
+        <span class="meta-sep"> | </span>
+        <span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'your-theme'); ?></span>
+        <span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
+        <?php edit_post_link( __( 'Edit', 'your-theme' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
+      </div><!-- .entry-meta -->
+
+
+      <h3>Actions</h3>
+      <ul class="actions">
+
+        <li class="with_icon">
+          <img alt="rss" src="http://s1.wordpress.com/wp-content/themes/pub/freshy/images/icons/feed-icon-16x16.gif" class="icon">&nbsp;
+          <a href="<?php comments_rss() ?>">Comments RSS</a>
+        </li>
+        <?php if ('open' == $post->ping_status) : // Comments or trackbacks open ?>  
+        <li class="with_icon">
+          <img alt="trackback" src="http://s1.wordpress.com/wp-content/themes/pub/freshy/images/icons/trackback-icon-16x16.gif" class="icon">&nbsp;
+          <a title="make a trackback" rel="trackback" href="<?php get_trackback_url() ?>">Trackback</a>
+        </li>
+        <?php endif; ?>
+      </ul>
+      <h3>Information</h3>
+      <ul class="information">
+        <li>
+          <span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
+        </li>
+      </ul>
+
+
+
+      <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
+      </div><!-- .entry-content -->
+                                        
+      <div class="entry-utility">
+        <?php printf( __( 'This entry was posted in %1$s%2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%5$s" title="Comments RSS to %4$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'your-theme' ),
+            get_the_category_list(', '),
+            get_the_tag_list( __( ' and tagged ', 'your-theme' ), ', ', '' ),
+            get_permalink(),
+            the_title_attribute('echo=0'),
+            comments_rss() ) ?>
+
+        <?php if ( ('open' == $post->comment_status) && ('open' == $post->ping_status) ) : // Comments and trackbacks open ?>
+          <?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'your-theme' ), get_trackback_url() ) ?>
+        <?php elseif ( !('open' == $post->comment_status) && ('open' == $post->ping_status) ) : // Only trackbacks open ?>
+          <?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'your-theme' ), get_trackback_url() ) ?>
+        <?php elseif ( ('open' == $post->comment_status) && !('open' == $post->ping_status) ) : // Only comments open ?>
+          <?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'your-theme' ) ?>
+        <?php elseif ( !('open' == $post->comment_status) && !('open' == $post->ping_status) ) : // Comments and trackbacks closed ?>
+          <?php _e( 'Both comments and trackbacks are currently closed.', 'your-theme' ) ?>
+        <?php endif; ?>
+        <?php edit_post_link( __( 'Edit', 'your-theme' ), "\n\t\t\t\t\t<span class=\"edit-link\">", "</span>" ) ?>
+      </div><!-- .entry-utility -->
+    </div><!-- #post-<?php the_ID(); ?> -->
                                 
-                                <div id="nav-below" class="navigation">
-                                        <div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&laquo;</span> %title' ) ?></div>
-                                        <div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&raquo;</span>' ) ?></div>
-                                </div><!-- #nav-below -->                                       
+    <div id="nav-below" class="navigation">
+      <div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&laquo;</span> %title' ) ?></div>
+      <div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&raquo;</span>' ) ?></div>
+    </div><!-- #nav-below -->
 
-<?php comments_template('', true); ?>                   
+    <?php comments_template('', true); ?>
                         
-                        </div><!-- #content -->         
-                </div><!-- #container -->
+  </div><!-- #content -->
+</div><!-- #container -->
                 
 <?php get_sidebar(); ?> 
 <?php get_footer(); ?>
